dfb87f3f35f4d396d9a59cbd9baa0fd1ae621783
Joseph Marhee <jmarhee@interiorae.com>
2019-09-06 01:35:49 -0500

    adds dl script

---
+++ dl.ps1
@@ -0,0 +1,28 @@
+param(
+  [string]$url,
+  [Parameter(Mandatory = $false )]
+  [string]$dest
+)
+
+Write-Host "Starting " $url "..."
+
+$client = new-object System.Net.WebClient
+
+if (!$dest)
+{
+    $dest = (Get-Item -Path ".\" -Verbose).FullName + "\" + [System.IO.Path]::GetFileNameWithoutExtension($url) + [System.IO.Path]::GetExtension($url)
+    Write-Host $dest
+} else {
+    Write-Host $dest
+}
+
+$default = (Get-Item -Path ".\" -Verbose).FullName + "\Downloads\" + $url
+
+function DownloadPath($url, $dest = $default) {	
+
+	Write-Host "Downloading to " $dest
+
+	$client.DownloadFile($url,$dest)
+}
+
+DownloadPath $url $dest
