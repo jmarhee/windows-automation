09ee45e16389a57bd55e05cedcd021310be8f5ff
Joseph D. Marhee <jmarhee@interiorae.com>
2020-01-28 00:59:05 -0600

    Adds WSL install function and check for Server 2019

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,7 +1,8 @@
 param(
   [Parameter(Mandatory = $false)] [switch]$choco = $false,
   [Parameter(Mandatory = $false)] [switch]$docker = $false,
-  [Parameter(Mandatory = $false)] [switch]$rdp = $false
+  [Parameter(Mandatory = $false)] [switch]$rdp = $false,
+  [Parameter(Mandatory = $false)] [switch]$wsl = $false
 )
 
 $errorActionPreference='Stop';
@@ -26,4 +27,23 @@ if ($docker)
 if ($rdp)
 {
     Cscript C:\Windows\system32\SCRegEdit.wsf /ar 0 ; Write-Host "Enabling RDP"
+} ;
+if ($wsl)
+{
+    $winVerName = systeminfo | findstr /B /C:"OS Name" 
+    $compatVersion = $winVerName -Like "*Server 2019*"
+    if ($compatVersion -eq "True")
+    {
+	Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+	$WslReb = Read-Host -Prompt 'WSL installed. Reboot may be required. Reboot now? [yes/no]';
+	if($WslReb -eq "yes")
+	{
+		Restart-Computer -Force
+	} else {
+		Write-Host "Computer will not reboot. Run 'Restart-Computer -Force' when you are ready to complete installation."
+	}
+    } else {
+	Write-Host "You are running the following version of Windows:"
+        Write-Host $winVerName
+    }
 }
